6d39fbfa104c0fd3771683de044e91ca249edda1
Brian Katzung <briank@kappacs.com>
2013-07-07 23:51:03 -0500

    Updates to clean up yardoc formatting.

--- lib/xkeys.rb
+++ lib/xkeys.rb
@@ -1,6 +1,10 @@
 # XKeys - Extended keys to facilitate fetching and storing in nested
 #	hash and array structures with Perl-ish auto-vivification.
 #
+# Synopsis:
+#  root = {}.extend XKeys::Hash
+#  root = [].extend XKeys::Auto
+#
 # Author:: Brian Katzung <briank@kappacs.com>, Kappa Computer Solutions, LLC
 # Copyright:: 2013 Brian Katzung and Kappa Computer Solutions, LLC
 # License:: MIT License
@@ -13,11 +17,11 @@ module XKeys::Get
     # Perform an extended fetch using successive keys to traverse a tree
     # of nested hashes and/or arrays.
     #
-    # xfetch([nil,] key1, ..., keyN [, :else => default_value])
+    #   xfetch([nil,] key1, ..., keyN [, :else => default_value])
     #
-    # An optional leading nil key is ignored (see []). If the specified
-    # keys do not exist, the default value is returned (if provided) or
-    # the standard exception (e.g. KeyError or IndexError) is raised.
+    #   An optional leading nil key is ignored (see []). If the specified
+    #   keys do not exist, the default value is returned (if provided) or
+    #   the standard exception (e.g. KeyError or IndexError) is raised.
     def xfetch (*args)
 	if args[-1].is_a?(Hash) then options, last = args[-1], -2
 	else options, last = {}, -1
@@ -35,17 +39,17 @@ module XKeys::Get
     # Perform an extended get using successive keys to traverse a tree of
     # nested hashes and/or arrays.
     #
-    # [key] returns the hash or array element (or range-based array slice)
-    # as normal.
+    #   [key] returns the hash or array element (or range-based array slice)
+    #   as normal.
     #
-    # array[int1, int2] returns a length-based array slice as normal.
-    # Prepend a nil key and/or append an option hash to force nested index
-    # behavior for two integer array indexes: array[nil, index1, index2].
+    #   array[int1, int2] returns a length-based array slice as normal.
+    #   Prepend a nil key and/or append an option hash to force nested index
+    #   behavior for two integer array indexes: array[nil, index1, index2].
     #
-    # [[nil,] key1, ..., keyN[, option_hash]] traverses a tree of nested
-    # hashes and/or arrays using xfetch. The optional leading nil key is
-    # always ignored. In the absence of an option hash, the default is
-    # :else => nil.
+    #   [[nil,] key1, ..., keyN[, option_hash]] traverses a tree of nested
+    #   hashes and/or arrays using xfetch. The optional leading nil key is
+    #   always ignored. In the absence of an option hash, the default is
+    #   :else => nil.
     def [] (*args)
 	if args.count == 1 || (self.is_a?(Array) && args.count == 2 &&
 	  args[0].is_a?(Integer) && args[1].is_a?(Integer))
@@ -58,6 +62,7 @@ module XKeys::Get
 
 end
 
+# "Private" module for XKeys::Set_* common code
 module XKeys::Set_
 
     # Common code for XKeys::Set_Hash and XKeys::Set_Auto
@@ -95,7 +100,7 @@ module XKeys::Set_Hash
     # assignment syntax. Nil keys create nested arrays as needed. Other
     # keys, including integer keys, create nested hashes as needed.
     #
-    # root[key1, ..., keyN] = value
+    #   root[key1, ..., keyN] = value
     def []= (*args)
 	super *args unless _xset(*args) { |key| key == nil }
 	args[-1]
@@ -112,7 +117,7 @@ module XKeys::Set_Auto
     # created nested arrays as needed. Other keys create nested hashes
     # as needed.
     #
-    # root[key1, ..., keyN] = value
+    #   root[key1, ..., keyN] = value
     def []= (*args)
 	super *args unless
 	  _xset(*args) { |key| key == nil || key.is_a?(Integer) }
@@ -123,8 +128,10 @@ end
 
 # Combined interfaces
 
+# XKeys::Hash combines XKeys::Get and XKeys::Set_Hash
 module XKeys::Hash; include XKeys::Get; include XKeys::Set_Hash; end
 
+# XKeys::Auto combines XKeys::Get and XKeys::Set_Auto
 module XKeys::Auto; include XKeys::Get; include XKeys::Set_Auto; end
 
 # END
